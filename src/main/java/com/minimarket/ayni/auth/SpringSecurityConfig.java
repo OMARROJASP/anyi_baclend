43cf51c3b34cc4d1ad6c1c9c61d45fd7c4070b32
OMARROJASP <63993140+OMARROJASP@users.noreply.github.com>
2024-01-14 17:04:26 -0500

    agregar el filter

--- src/main/java/com/minimarket/ayni/auth/SpringSecurityConfig.java
+++ src/main/java/com/minimarket/ayni/auth/SpringSecurityConfig.java
@@ -1,14 +1,20 @@
 package com.minimarket.ayni.auth;
 
+import com.minimarket.ayni.auth.filter.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfig {
+
+    @Autowired
+    private AuthenticationConfiguration authenticationConfiguration;
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
@@ -17,6 +23,7 @@ public class SpringSecurityConfig {
                 .permitAll()
                 .anyRequest().authenticated()
                 .and()
+                .addFilter(new JwtAuthenticationFilter(authenticationConfiguration.getAuthenticationManager()))
                 .csrf(config-> config.disable())
                 .sessionManagement(managment -> managment.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
